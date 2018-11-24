4d530c3247b1ed9dace0918451d3a01ab9b98aa5
Jakob <s67742@beuth-hochschule.de>
2018-11-24 17:21:26 +0100

    added provisional scripts for SpaceCraftControl and Attractor (Newtons Gravity Law)

---
+++ SpaceRush/Assets/Scripts/Attractor.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour
+{
+
+    public static List<Attractor> Attractors;
+    public bool attracted = true;
+    private Rigidbody2D rb;
+    const float G = 0.6674f;
+
+    void OnEnable()
+    {
+        if (Attractors == null)
+        {
+            Attractors = new List<Attractor>();
+        }
+        rb = GetComponent<Rigidbody2D>();
+        Attractors.Add(this);
+    }
+
+    void OnDisable()
+    {
+        Attractors.Remove(this);
+    }
+
+    void FixedUpdate()
+    {
+        foreach (Attractor attractor in Attractors)
+        {
+            if (attractor != this)
+            {
+                if (attractor.attracted)
+                {
+                    Attract(attractor);
+                }
+            }
+        }
+    }
+
+    void Attract(Attractor toAttract)
+    {
+        Rigidbody2D rbToAttract = toAttract.rb;
+
+        Vector2 direction = rb.position - rbToAttract.position;
+        float distance = direction.magnitude;
+
+        if (distance == 0)
+        {
+            return;
+        }
+
+        float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+        Vector2 force = direction.normalized * forceMagnitude;
+
+        rbToAttract.AddForce(force);
+    }
+}
