513b956b393b3a89b46a50c485325daf0a578d1f
Jakob <s67742@beuth-hochschule.de>
2018-11-26 11:10:51 +0100

    added the real Vector2Extensions file

---
+++ SpaceRush/Assets/Scripts/Vector2Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector2Extensions : MonoBehaviour
+{
+    public static Vector2 Rotate(Vector2 vec, float angle)
+    {
+        float newX = (vec.x * Mathf.Cos(angle)) - (vec.y * Mathf.Sin(angle));
+        float newY = (vec.x * Mathf.Sin(angle)) + (vec.y * Mathf.Cos(angle));
+        return new Vector2(newX, newY);
+    }
+}
