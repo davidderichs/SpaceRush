bdcc105c245b3f57d3bd625d4a49d67b0ad2e690
s67710 <s67710@beuth-hochschule.de>
2018-12-02 09:55:37 +0100

    Endpoint added, needs funktions

---
+++ SpaceRush/Assets/Scripts/Endpoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Endpoint : MonoBehaviour {
+	public Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+		position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void OnTriggerEnter()
+    {
+            Debug.Log("entered");
+    }
+}
--- SpaceRush/Assets/Scripts/Movements.cs
+++ SpaceRush/Assets/Scripts/Movements.cs
@@ -3,11 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movements : MonoBehaviour {
-	public SpaceCraftControl spaceCraft;
-	public int blevel1 = 100;
-	public int blevel2 = 200;
-	public int blevel3 = 300;
-	public int bdir = 5;
+	public SpacecraftController spacecraft;
 
 
 	// Use this for initialization
@@ -17,31 +13,17 @@ public class Movements : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-         if (Input.GetKeyDown(KeyCode.A)){
-			 spaceCraft.AddBoost(SpaceCraftControl.Booster.FORWARD,blevel1);
+         if (Input.GetKeyDown(KeyCode.W)){
+			 spacecraft.BoostForwards(10);
 		 }
 		 if (Input.GetKeyDown(KeyCode.S)){
-			 spaceCraft.AddBoost(SpaceCraftControl.Booster.FORWARD,blevel2);
+			 spacecraft.BoostBackwards(10);
+		 }
+		 if (Input.GetKeyDown(KeyCode.A)){
+			 spacecraft.Rotate(4,SpacecraftController.Direction.Left);
 		 }
 		 if (Input.GetKeyDown(KeyCode.D)){
-			 spaceCraft.AddBoost(SpaceCraftControl.Booster.FORWARD,blevel3);
-		 }
-		 if (Input.GetKeyDown(KeyCode.Y)){
-			 spaceCraft.AddBoost(SpaceCraftControl.Booster.BACKWARD,blevel1);
-		 }
-		 if (Input.GetKeyDown(KeyCode.X)){
-			 spaceCraft.AddBoost(SpaceCraftControl.Booster.BACKWARD,blevel2);
-		 }
-		 if (Input.GetKeyDown(KeyCode.C)){
-			 spaceCraft.AddBoost(SpaceCraftControl.Booster.BACKWARD,blevel3);
-		 }
-		 //90° wende nach Rechts
-		 if (Input.GetKeyDown(KeyCode.F)){
-			 spaceCraft.AddBoost(SpaceCraftControl.Booster.LEFT, bdir);
-		 }
-		 // 90° wende nach Links
-		 if (Input.GetKeyDown(KeyCode.G)){
-			 spaceCraft.AddBoost(SpaceCraftControl.Booster.RIGHT, bdir);
+			 spacecraft.Rotate(4,SpacecraftController.Direction.Right);
 		 }
 	}
 
