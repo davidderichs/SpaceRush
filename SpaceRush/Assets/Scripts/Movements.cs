a47c24633218200304129b716232e6d900fb4724
Jakob <s67742@beuth-hochschule.de>
2018-12-02 13:13:39 +0100

    adjusted Movements

--- SpaceRush/Assets/Scripts/Movements.cs
+++ SpaceRush/Assets/Scripts/Movements.cs
@@ -2,35 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movements : MonoBehaviour {
-	public SpacecraftController spacecraft;
+public class Movements : MonoBehaviour
+{
+    public SpacecraftController spacecraft;
 
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-         if (Input.GetKeyDown(KeyCode.W)){
-			 spacecraft.BoostForwards(10);
-		 }
-		 if (Input.GetKeyDown(KeyCode.S)){
-			 spacecraft.BoostBackwards(10);
-		 }
-		 if (Input.GetKeyDown(KeyCode.A)){
-			 spacecraft.Rotate(4,SpacecraftController.Direction.Left);
-		 }
-		 if (Input.GetKeyDown(KeyCode.D)){
-			 spacecraft.Rotate(4,SpacecraftController.Direction.Right);
-		 }
-		 if (Input.GetKeyDown(KeyCode.Q)){
-			 spacecraft.Rotate(2,SpacecraftController.Direction.Left);
-		 }
-		 if (Input.GetKeyDown(KeyCode.E)){
-			 spacecraft.Rotate(2,SpacecraftController.Direction.Right);
-		 }
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            spacecraft.BoostForwards(1);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            spacecraft.BoostBackwards(1);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            spacecraft.Rotate(1, SpacecraftController.Direction.Left);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            spacecraft.Rotate(1, SpacecraftController.Direction.Right);
+        }
+    }
 
 }
