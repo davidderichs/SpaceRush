0a486f941ede3b95e2b344e7926317a217dbfa7b
Jakob <s67742@beuth-hochschule.de>
2018-12-03 12:33:31 +0100

    Endpoint script gefixed

--- SpaceRush/Assets/Scripts/Endpoint.cs
+++ SpaceRush/Assets/Scripts/Endpoint.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Endpoint : MonoBehaviour {
-	public Vector3 position;
+public class Endpoint : MonoBehaviour
+{
+    public Vector3 position;
+    private Light pointLight;
 
-	// Use this for initialization
-	void Start() {
-		position = transform.position;
-	}
-	void OnTriggerEnter(Collider other)
+    void Start()
     {
-		Light point = GetComponent<Light>();
-		point.color = Color.green;
+        position = transform.position;
+        pointLight = GetComponent<Light>();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        pointLight.color = Color.green;
     }
 }
