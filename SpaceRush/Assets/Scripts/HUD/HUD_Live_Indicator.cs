84fe04f54b5d8f0fec6179036647f48335bc2fdf
davidderichs <s57343@beuth-hochschule.de>
2018-12-04 17:04:40 +0100

    HUD in Szene eingebaut. Sprites als PNG eingebunden.

---
+++ SpaceRush/Assets/Scripts/HUD/HUD_Live_Indicator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD_Live_Indicator : MonoBehaviour {
+
+	public Sprite[] images;
+
+	private Image imageScript;
+
+	private RectTransform canvasRectTransform;
+    private RectTransform panelRectTransform;
+
+
+    void Awake () {
+        Canvas canvas = GetComponentInParent <Canvas>();
+        if (canvas != null) {
+            canvasRectTransform = canvas.transform as RectTransform;
+            panelRectTransform = transform.parent as RectTransform;
+			panelRectTransform.sizeDelta = new Vector2 (1920, 1080);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
