a439b65930618cc851dfd6767394b69e13f9508a
s67710 <s67710@beuth-hochschule.de>
2018-12-04 14:42:54 +0100

    Checkpoints and Reset added

--- SpaceRush/Assets/Scripts/Checkpoint.cs
+++ SpaceRush/Assets/Scripts/Checkpoint.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour {
 	private Collider2D coll;
+	private Light point;
 	public int id;
 	public GameManager game;
 
 	// Use this for initialization
 	void Start () {
 		coll = GetComponent<Collider2D>();
+		point = GetComponent<Light>();
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
-		if (other.gameObject.tag == "MyGameObjectTag"){
-
+		if (other.gameObject.tag == "Space"){
+			point.color = Color.green;
+			game.checkpointTriggered(id, game.pl1);
 		}
-		//game.checkpointTriggered(id, );
 	}
 }
