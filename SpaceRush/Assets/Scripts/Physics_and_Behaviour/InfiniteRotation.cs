49cc51b9f22e0aa8bcda0dcb269d329fa270faa1
Jakob <s67742@beuth-hochschule.de>
2018-12-12 16:03:15 +0100

    renamed Rotation to InfiniteRotation

--- SpaceRush/Assets/Scripts/Physics_and_Behaviour/InfiniteRotation.cs
+++ SpaceRush/Assets/Scripts/Physics_and_Behaviour/InfiniteRotation.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotation : MonoBehaviour
+public class InfiniteRotation : MonoBehaviour
 {
 
     public float fullRotationTime;
