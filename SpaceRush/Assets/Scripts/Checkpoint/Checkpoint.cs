ea209e3748097725fa5f453589c95aafdc32625e
Jakob <s67742@beuth-hochschule.de>
2018-12-16 22:44:57 +0100

    checkpoint labels, spacecraft labelled

--- SpaceRush/Assets/Scripts/Checkpoint/Checkpoint.cs
+++ SpaceRush/Assets/Scripts/Checkpoint/Checkpoint.cs
@@ -2,22 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Checkpoint : MonoBehaviour {
-	private Collider2D coll;
-	private Light point;
-	public int id;
-	public GameManager game;
+public class Checkpoint : MonoBehaviour
+{
+    private Collider2D coll;
+    private Light point;
+    public int id;
+    public GameManager game;
 
-	// Use this for initialization
-	void Start () {
-		coll = GetComponent<Collider2D>();
-		point = GetComponent<Light>();
-	}
+    // Use this for initialization
+    void Start()
+    {
+        coll = GetComponent<Collider2D>();
+        point = GetComponent<Light>();
+    }
 
-	void OnTriggerEnter2D(Collider2D other){
-		if (other.gameObject.tag == "Space"){
-			point.color = Color.green;
-			game.checkpointTriggered(id, game.player_1);
-		}
-	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Spacecraft")
+        {
+            point.color = Color.green;
+            game.checkpointTriggered(id, game.player_1);
+        }
+    }
 }
