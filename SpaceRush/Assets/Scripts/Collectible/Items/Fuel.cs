167870ed54979f3a0ea964c8c92cea47b58691b2
s67710 <s67710@beuth-hochschule.de>
2019-01-14 11:16:11 +0100

    Fuelobject Fixed

--- SpaceRush/Assets/Scripts/Collectible/Items/Fuel.cs
+++ SpaceRush/Assets/Scripts/Collectible/Items/Fuel.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class Fuel : Collectible {
 
 	void OnTriggerEnter2D(Collider2D other){
-		Debug.Log("collected");
 		findGameManager();
-		
 		if (other.transform.gameObject.name == "Spacecraft"){
 			game.player_1.main_fuel = game.player_1.main_fuel + 5; 
 			Debug.Log("collected");
