b7054410bdecc835316e4d2205c8b0b00d6c4c72
s57343 <s57343@beuth-hochschule.de>
2019-01-19 13:35:12 +0100

    Main Menu:    Menu UI Assets added    Scene MainMenu added to control UI and Menu as well as starting other Scenes

--- SpaceRush/Assets/Scripts/Animator/Animator.cs
+++ SpaceRush/Assets/Scripts/Animator/Animator.cs
@@ -13,6 +13,9 @@ public class Animator
     private bool running;
     private Vector3 distanceVec;
 
+    public GameObject gameObject;
+    public Transform transform;
+
     public Animator()
     {
         animationTime = 0;
@@ -66,5 +69,33 @@ public class Animator
         distanceVec = new Vector3();
         currentTime = 0;
         running = false;
+    }
+
+    public static int StringToHash(string value){
+     int result = 0;
+     for (int i = 0; i < value.Length; i++)
+     {
+         char letter = value[i];
+         result = 10 * result + (letter - 48);
+     }
+     return result;
+    }
+
+    public void SetBool(int hash, bool value){
+        // Kein Plan, was hier passieren muss.
+    }
+
+    public bool GetBool(int value){
+        if (value <= 0 ) return false;
+        if (value > 0) return true;
+        return false;
+    }
+
+    public bool IsInTransition(int value){
+        return true;
+    }
+
+    public AnimatorTransitionInfo GetCurrentAnimatorStateInfo(int value){
+        return new AnimatorTransitionInfo();
     }
 }
