0af83252e2c677bf06aed40f7ded4b475714c3ca
s67710 <s67710@beuth-hochschule.de>
2019-01-27 16:33:41 +0100

    Carddesc fixed

--- SpaceRush/Assets/Scripts/Cards_and_Collectables/MoveCardCreator.cs
+++ SpaceRush/Assets/Scripts/Cards_and_Collectables/MoveCardCreator.cs
@@ -7,47 +7,47 @@ public class MoveCardCreator : MonoBehaviour
 
     public static MoveCard getForward()
     {
-        MoveCard card = new MoveCard("forward", 3, new Color(1, 1, 1, 1), true, "Sprites/", "boost", 2, 300);
+        MoveCard card = new MoveCard("forward", 1, new Color(1, 1, 1, 1), true, "Sprites/", "boost", 2, 300);
         return card;
     }
 
     public static MoveCard getBackward()
     {
-        MoveCard card = new MoveCard("backward", 3, new Color(1, 1, 1, 1), true, "Sprites/", "boost", 2, 300);
+        MoveCard card = new MoveCard("backward", 1, new Color(1, 1, 1, 1), true, "Sprites/", "boost", 2, 300);
         return card;
     }
     public static MoveCard getRotationRight30()
     {
-        MoveCard card = new MoveCard("Rotate_Right", 3, new Color(1, 1, 1, 1), true, "Sprites/", "rotate", 2, 15);
+        MoveCard card = new MoveCard("Rotate_Right", 30, new Color(1, 1, 1, 1), true, "Sprites/", "rotate", 2, 15);
         return card;
     }
     public static MoveCard getRotationLeft30()
     {
-        MoveCard card = new MoveCard("Rotate_Left", 3, new Color(1, 1, 1, 1), true, "Sprites/", "rotate", 2, 15);
+        MoveCard card = new MoveCard("Rotate_Left", 30, new Color(1, 1, 1, 1), true, "Sprites/", "rotate", 2, 15);
         return card;
     }
 
     public static MoveCard getRotationRight60()
     {
-        MoveCard card = new MoveCard("Rotate_Right", 3, new Color(1, 1, 1, 1), true, "Sprites/", "rotate", 2, 30);
+        MoveCard card = new MoveCard("Rotate_Right", 60, new Color(1, 1, 1, 1), true, "Sprites/", "rotate", 2, 30);
         return card;
     }
 
     public static MoveCard getRotationLeft60()
     {
-        MoveCard card = new MoveCard("Rotate_Left", 3, new Color(1, 1, 1, 1), true, "Sprites/", "rotate", 2, 30);
+        MoveCard card = new MoveCard("Rotate_Left", 60, new Color(1, 1, 1, 1), true, "Sprites/", "rotate", 2, 30);
         return card;
     }
 
     public static MoveCard getRotationRight90()
     {
-        MoveCard card = new MoveCard("Rotate_Right", 3, new Color(1, 1, 1, 1), true, "Sprites/", "rotate", 2, 45);
+        MoveCard card = new MoveCard("Rotate_Right", 90, new Color(1, 1, 1, 1), true, "Sprites/", "rotate", 2, 45);
         return card;
     }
 
     public static MoveCard getRotationLeft90()
     {
-        MoveCard card = new MoveCard("Rotate_Left", 3, new Color(1, 1, 1, 1), true, "Sprites/", "rotate", 2, 45);
+        MoveCard card = new MoveCard("Rotate_Left", 90, new Color(1, 1, 1, 1), true, "Sprites/", "rotate", 2, 45);
         return card;
     }
 
@@ -55,10 +55,19 @@ public class MoveCardCreator : MonoBehaviour
 		switch(weaponName){
 			case "Weapon_Laser": return getWeaponLaser();
 			case "Weapon_Gravity_Mine": return getWeaponGravityMine();
-			default: return getRotationRight90();
+			default: return getForwardFast();
 		}
 	}
 
+    private static MoveCard getForwardFast(){
+        MoveCard card = new MoveCard("forward", 2, new Color(1, 1, 1, 1), true, "Sprites/", "boost", 2, 500);
+        return card;
+    }
+
+    private static MoveCard getBackwardFast(){
+        MoveCard card = new MoveCard("backward", 2, new Color(1, 1, 1, 1), true, "Sprites/", "boost", 2, 500);
+        return card;
+    }
 
 	private static MoveCard getWeaponLaser()
     {
