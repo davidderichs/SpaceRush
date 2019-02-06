927b9175af4951b3a75c2f3fd6671258454c91a5
Jakob <Jakob@Jakobs-Mac-Pro.fritz.box>
2019-02-06 16:57:06 +0100

    added a blinking light to the spacecraft to indicate which is whos player

---
+++ SpaceRush/Assets/Scripts/BlinkLight/BlinkLight.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkLight : MonoBehaviour {
+
+	private double counter;
+	public double lightPhase;
+	public double offPhase;
+	public float intensity;
+	private bool on;
+	private Light light;
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light>();
+		counter = 0;
+		light.intensity = 0;
+		on = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		counter += Time.deltaTime;
+		if(on && counter >= lightPhase) {
+			light.intensity = 0;
+			on = false;
+			counter = 0;
+		} else if(!on && counter >= offPhase) {
+			light.intensity = intensity;
+			on = true;
+			counter = 0;
+		}
+	}
+}
