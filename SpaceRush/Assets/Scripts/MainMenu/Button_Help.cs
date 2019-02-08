68ea7c01a142328461aa8e30bc8638907229b6ff
s57343 <s57343@beuth-hochschule.de>
2019-02-08 15:06:02 +0100

    Added Tutorial Section in Main Menu

---
+++ SpaceRush/Assets/Scripts/MainMenu/Button_Help.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button_Help : MonoBehaviour {
+
+	private Button help_Button;
+	private GameObject mainMenu_Panel;
+	private GameObject mainMenu_Tutorial_Canvas;
+
+	// Use this for initialization
+	void Start () {
+		this.help_Button = GameObject.Find("Button_Help").GetComponent<Button>();
+		this.mainMenu_Panel = GameObject.Find("MainMenu_Panel");
+		this.mainMenu_Tutorial_Canvas = GameObject.Find("Tutorial_Canvas");
+		this.mainMenu_Tutorial_Canvas.SetActive(false);
+
+		help_Button.onClick.AddListener(delegate{
+			if(this.mainMenu_Panel.activeSelf){
+				this.mainMenu_Panel.SetActive(false);
+				this.mainMenu_Tutorial_Canvas.SetActive(true);
+				this.help_Button.GetComponentInChildren<Text>().text = "Close";
+			} else {
+				this.mainMenu_Panel.SetActive(true);
+				this.mainMenu_Tutorial_Canvas.SetActive(false);
+				this.help_Button.GetComponentInChildren<Text>().text = "Game Rules";
+			}
+			
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- SpaceRush/Assets/Scripts/MainMenu/Button_Settings.cs
+++ SpaceRush/Assets/Scripts/MainMenu/Button_Settings.cs
@@ -29,44 +29,64 @@ public class Button_Settings : MonoBehaviour {
 		player_1_settings_spacecraft = GameObject.Find("spacecraft_player_1");
 		player_2_settings_spacecraft = GameObject.Find("spacecraft_player_2");
 
+		// Blue
 		Player_Stats.Player_1_color = new Color32(0,29,226,255);
+		// Yellow
 		Player_Stats.Player_2_color = new Color32(195,170,0,255);
 
 		player_1_blue = GameObject.Find("settings_player_1_color_blue").GetComponent<Button>();
-		player_1_red = GameObject.Find("settings_player_1_color_red").GetComponent<Button>();
+		highlight_Button_player_1(player_1_blue);
+		player_1_red = GameObject.Find("settings_player_1_color_red").GetComponent<Button>();		
 		player_1_yellow = GameObject.Find("settings_player_1_color_yellow").GetComponent<Button>();
 		player_1_green = GameObject.Find("settings_player_1_color_green").GetComponent<Button>();
 
 		player_2_blue = GameObject.Find("settings_player_2_color_blue").GetComponent<Button>();
 		player_2_red = GameObject.Find("settings_player_2_color_red").GetComponent<Button>();
 		player_2_yellow = GameObject.Find("settings_player_2_color_yellow").GetComponent<Button>();
+		highlight_Button_player_2(player_2_yellow);
 		player_2_green = GameObject.Find("settings_player_2_color_green").GetComponent<Button>();
 
 
 		player_1_blue.onClick.AddListener(delegate{
 			player_1_Color(new Color32(0,29,226,255));
+			un_highlight_Buttons_player_1();
+			highlight_Button_player_1(player_1_blue);
 		});
 		player_1_red.onClick.AddListener(delegate{
 			player_1_Color(new Color32(255,0,0,255));
+			un_highlight_Buttons_player_1();
+			highlight_Button_player_1(player_1_red);
 		});
 		player_1_yellow.onClick.AddListener(delegate{
 			player_1_Color(new Color32(195,170,0,255));
+			un_highlight_Buttons_player_1();
+			highlight_Button_player_1(player_1_yellow);
 		});
 		player_1_green.onClick.AddListener(delegate{
 			player_1_Color(new Color32(10,166,0,255));
+			un_highlight_Buttons_player_1();
+			highlight_Button_player_1(player_1_green);
 		});
 
 		player_2_blue.onClick.AddListener(delegate{
-			player_2_Color(new Color32(0,29,226,255));
+			player_2_Color(new Color32(0,29,226,255));			
+			un_highlight_Buttons_player_2();
+			highlight_Button_player_2(player_2_blue);
 		});
 		player_2_red.onClick.AddListener(delegate{
-			player_2_Color(new Color32(255,0,0,255));
+			player_2_Color(new Color32(255,0,0,255));			
+			un_highlight_Buttons_player_2();
+			highlight_Button_player_2(player_2_red);
 		});
 		player_2_yellow.onClick.AddListener(delegate{
-			player_2_Color(new Color32(195,170,0,255));
+			player_2_Color(new Color32(195,170,0,255));			
+			un_highlight_Buttons_player_2();
+			highlight_Button_player_2(player_2_yellow);
 		});
 		player_2_green.onClick.AddListener(delegate{
-			player_2_Color(new Color32(10,166,0,255));
+			player_2_Color(new Color32(10,166,0,255));			
+			un_highlight_Buttons_player_2();
+			highlight_Button_player_2(player_2_green);
 		});
 
 
@@ -79,8 +99,30 @@ public class Button_Settings : MonoBehaviour {
 	}
 
 	void Update(){
-		player_1_settings_spacecraft.GetComponent<MeshRenderer>().material.color = Player_Stats.Player_1_color;
-		player_2_settings_spacecraft.GetComponent<MeshRenderer>().material.color = Player_Stats.Player_2_color;
+		player_1_settings_spacecraft.GetComponentsInChildren<Light>()[0].color = Player_Stats.Player_1_color;
+		player_2_settings_spacecraft.GetComponentsInChildren<Light>()[0].color = Player_Stats.Player_2_color;
+	}
+
+	void un_highlight_Buttons_player_1(){
+		player_1_blue.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+		player_1_red.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+		player_1_yellow.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+		player_1_green.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+	}
+
+	void un_highlight_Buttons_player_2(){
+		player_2_blue.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+		player_2_red.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+		player_2_yellow.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+		player_2_green.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+	}
+
+	void highlight_Button_player_1(Button clickedButton){
+		clickedButton.transform.localScale += new Vector3(0.2f, 0.2f, 0.0f);
+	}
+
+	void highlight_Button_player_2(Button clickedButton){
+		clickedButton.transform.localScale += new Vector3(0.2f, 0.2f, 0.0f);
 	}
 
 	void player_1_Color(Color32 color){
