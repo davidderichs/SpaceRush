15cee2d226ef8eadce43194e263b6de2c89a934b
Jakob <s67742@beuth-hochschule.de>
2019-02-08 13:37:31 +0100

    added missile weapon

---
+++ SpaceRush/Assets/Scripts/Weapon/Weapons/Missile/States/MissileMovingState.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileMovingState : WeaponState
+{
+    private Rigidbody2D rb;
+    private float acceleration;
+
+    public MissileMovingState(Weapon weapon) : base(weapon)
+    { }
+
+    public override void Tick()
+    {
+        rb.AddRelativeForce(new Vector2(acceleration, 0));
+    }
+
+    public override void OnStateEnter()
+    {
+        rb = weapon.GetComponent<Rigidbody2D>();
+        acceleration = 10;
+    }
+
+    public override void OnStateExit()
+    {
+
+    }
+}
