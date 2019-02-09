88550ae2966e5ee60895b14e67b931645d27a0ee
Jakob <s67742@beuth-hochschule.de>
2019-02-09 17:08:09 +0100

    started updating the checkpoints

--- SpaceRush/Assets/Scripts/Checkpoint/Checkpoint.cs
+++ SpaceRush/Assets/Scripts/Checkpoint/Checkpoint.cs
@@ -4,48 +4,33 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    private Light point;
+    private GameObject indicator1;
+    private GameObject indicator2;
     public int id;
     private GameManager game;
 
     // Use this for initialization
     void Start()
     {
-        point = GetComponent<Light>();
+        indicator1 = transform.Find("indicator1").gameObject;
+        indicator2 = transform.Find("indicator2").gameObject;
         game = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.transform.gameObject.name == "Spacecraft")
+        if (other.transform.gameObject.name == "Spacecraft1")
         {
-            if (point.color == Color.red)
-            {
-                point.color = game.player_1.playerColor;
-                game.checkpointTriggered(id, game.player_1);
-            }
-            else
-            {
-                point.color = Color.green;
-                game.checkpointTriggered(id, game.player_1);
-
-            }
-
+            indicator1.GetComponent<Renderer>().material.SetColor("Color_DDEDA7A6", game.player_1.playerColor);
+            indicator1.SetActive(true);
+            game.checkpointTriggered(id, game.player_1);
         }
 
         if (other.transform.gameObject.name == "Spacecraft2")
         {
-           if (point.color == Color.red)
-            {
-                point.color = game.player_1.playerColor;
-                game.checkpointTriggered(id, game.player_2);
-            }
-            else
-            {
-                point.color = Color.green;
-                game.checkpointTriggered(id, game.player_2);
-
-            }
+            indicator2.GetComponent<Renderer>().material.SetColor("Color_DDEDA7A6", game.player_2.playerColor);
+            indicator2.SetActive(true);
+            game.checkpointTriggered(id, game.player_2);
         }
     }
 }
