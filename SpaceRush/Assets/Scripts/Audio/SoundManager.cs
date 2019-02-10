b2087cb1a14823612fb4ed7fdcbb3898d45abf52
s57343 <s57343@beuth-hochschule.de>
2019-02-11 00:47:39 +0100

    Weapon Sounds added and implemented in SoundManager

--- SpaceRush/Assets/Scripts/Audio/SoundManager.cs
+++ SpaceRush/Assets/Scripts/Audio/SoundManager.cs
@@ -15,6 +15,10 @@ public class SoundManager : MonoBehaviour {
 	private UnityAction spacecraft_collision_listener;
 	private UnityAction spacecraft_boost_start_listener;
 	private UnityAction spacecraft_boost_stop_listener;
+	private UnityAction gravity_mine_put_listener;
+	private UnityAction gravity_mine_explosion_listener;
+	private UnityAction missile_launch_listener;
+	private UnityAction missile_explosion_listener;
 	private UnityAction simulation_start_listener;
 	private UnityAction simulation_stop_listener;
 
@@ -53,6 +57,18 @@ public class SoundManager : MonoBehaviour {
 
 		simulation_start_listener = new UnityAction (simulation_start_routine);
 		EventManager.StartListening("start_simulation", simulation_start_listener);
+
+		gravity_mine_put_listener = new UnityAction (gravity_mine_put);
+		EventManager.StartListening("gravitymine_put", gravity_mine_put_listener);
+
+		gravity_mine_explosion_listener = new UnityAction (gravity_mine_explosion);
+		EventManager.StartListening("gravitymine_explosion", gravity_mine_explosion_listener);
+
+		missile_launch_listener = new UnityAction (missile_launch);
+		EventManager.StartListening("missile_fired", missile_launch_listener);
+
+		missile_explosion_listener = new UnityAction (missile_explosion);
+		EventManager.StartListening("missile_explosion", missile_explosion_listener);
 	}
 	
 	// Update is called once per frame
@@ -62,6 +78,22 @@ public class SoundManager : MonoBehaviour {
 	void simulation_start_routine(){
 		boostSoundStopped = false;
 	}
+
+	void missile_launch(){
+		GameObject.Find("Audio_Source_Missile_Launch").GetComponent<AudioSource>().Play();
+	}
+
+	void missile_explosion(){
+		GameObject.Find("Audio_Source_Gravity_Mine_Explosion").GetComponent<AudioSource>().Play();
+	}
+	void gravity_mine_put(){
+		GameObject.Find("Audio_Source_Gravity_Mine_Put").GetComponent<AudioSource>().Play();
+	}
+
+	void gravity_mine_explosion(){
+		GameObject.Find("Audio_Source_Gravity_Mine_Explosion").GetComponent<AudioSource>().Play();
+	}
+
 	void stop_simulation_reladed_sounds(){
 		GameObject.Find("Audio_Source_Spacecraft_Boost").GetComponent<AudioSource>().Stop();
 	}
@@ -85,17 +117,12 @@ public class SoundManager : MonoBehaviour {
 	void play_sound_spacecraft_collision(){
 		GameObject.Find("Audio_Source_Spacecraft_Collision").GetComponent<AudioSource>().Play();
 	}
-	void play_sound_spacecraft_boost(){		
-		if(!this.boostSoundStopped){
-			if (!GameObject.Find("Audio_Source_Spacecraft_Boost").GetComponent<AudioSource>().isPlaying){
-				Debug.Log("Starting Boost Sound");
-				GameObject.Find("Audio_Source_Spacecraft_Boost").GetComponent<AudioSource>().Play();
-			}	
-		}	
+	void play_sound_spacecraft_boost(){	
+		if (!GameObject.Find("Audio_Source_Spacecraft_Boost").GetComponent<AudioSource>().isPlaying){
+			GameObject.Find("Audio_Source_Spacecraft_Boost").GetComponent<AudioSource>().Play();
+		}
 	}
 	void stop_sound_spacecraft_boost(){
-		Debug.Log("############## Stopping Boost Sound");
 		GameObject.Find("Audio_Source_Spacecraft_Boost").GetComponent<AudioSource>().Stop();
-		this.boostSoundStopped = true;
 	}
 }
