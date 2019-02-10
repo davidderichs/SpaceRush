99adecaae06ea95bf20e844e42c4263eeb6a6109
s67710 <s67710@beuth-hochschule.de>
2019-02-10 17:09:28 +0100

    full Fixed Stuff

--- SpaceRush/Assets/Scripts/Collectible/Weapons/Missile.cs
+++ SpaceRush/Assets/Scripts/Collectible/Weapons/Missile.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Laser : Collectible {
+public class Missile : Collectible {
     void OnTriggerEnter2D(Collider2D other){
 		findGameManager();
 		if (other.transform.gameObject.name == "Spacecraft1"){
-			game.player_1.addWeapon("Weapon_Laser");
+			game.player_1.addWeapon("rocket");
 			//Debug.Log("collected");
 		}
 		if (other.transform.gameObject.name == "Spacecraft2"){
-			game.player_2.addWeapon("Weaon_Laser");
+			game.player_2.addWeapon("rocket");
 			//Debug.Log("collected");
 		}
 		collected();
