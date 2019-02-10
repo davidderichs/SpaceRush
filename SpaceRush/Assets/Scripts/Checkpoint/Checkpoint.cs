852d7683a18191e1e4ebe9f66328d90804a45604
s67710 <s67710@beuth-hochschule.de>
2019-02-10 20:43:47 +0100

    fixed

--- SpaceRush/Assets/Scripts/Checkpoint/Checkpoint.cs
+++ SpaceRush/Assets/Scripts/Checkpoint/Checkpoint.cs
@@ -8,6 +8,7 @@ public class Checkpoint : MonoBehaviour
     private GameObject indicator2;
     public int id;
     public bool isFull;
+    public bool enable;
     private GameManager game;
 
     // Use this for initialization
@@ -15,29 +16,32 @@ public class Checkpoint : MonoBehaviour
     {
         indicator1 = transform.Find("indicator1").gameObject;
         indicator2 = transform.Find("indicator2").gameObject;
-
+        enable = false;
         isFull = false;
         game = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.transform.gameObject.name == "Spacecraft1" && other.transform.gameObject.tag == "spacecraft")
+        if (enable)
         {
-            Debug.Log("Id" + id);
-            indicator1.SetActive(true);
-            indicator1.GetComponent<Renderer>().material.SetColor("Color_DDEDA7A6", game.player_1.playerColor);
-            game.player_1.addCheckpoint(id);
-            isFull = true;
-        }
+            if (other.transform.gameObject.name == "Spacecraft1" && other.transform.gameObject.tag == "spacecraft")
+            {
+                Debug.Log("Id" + id);
+                indicator1.SetActive(true);
+                indicator1.GetComponent<Renderer>().material.SetColor("Color_DDEDA7A6", game.player_1.playerColor);
+                game.player_1.addCheckpoint(id);
+                isFull = true;
+            }
 
-        if (other.transform.gameObject.name == "Spacecraft2" && other.transform.gameObject.tag == "spacecraft")
-        {
-            Debug.Log("id:" + id);
-            indicator2.SetActive(true);
-            indicator2.GetComponent<Renderer>().material.SetColor("Color_DDEDA7A6", game.player_2.playerColor);
-            game.player_2.addCheckpoint(id);
-            isFull = true;
+            if (other.transform.gameObject.name == "Spacecraft2" && other.transform.gameObject.tag == "spacecraft")
+            {
+                Debug.Log("id:" + id);
+                indicator2.SetActive(true);
+                indicator2.GetComponent<Renderer>().material.SetColor("Color_DDEDA7A6", game.player_2.playerColor);
+                game.player_2.addCheckpoint(id);
+                isFull = true;
+            }
         }
     }
     void OnTriggerStay2D(Collider2D other)
