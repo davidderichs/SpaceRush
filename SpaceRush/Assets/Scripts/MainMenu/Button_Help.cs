86b5a032447ed903332fc2e360b90e9832179278
s67710 <s67710@beuth-hochschule.de>
2019-02-10 21:36:09 +0100

    fixed

--- SpaceRush/Assets/Scripts/MainMenu/Button_Help.cs
+++ SpaceRush/Assets/Scripts/MainMenu/Button_Help.cs
@@ -97,7 +97,7 @@ public class Button_Help : MonoBehaviour
             this.un_highlight_Tutorial_Buttons();
             this.highlight_Tutorial_Button(tutorial_Button_Checkpoints);
             this.change_tutorial_headline("Checkpoints");
-            this.change_tutorial_text("If a checkpoint is reached they'll be marked with the colour of the player. Last reached checkpoint is used as a respawn point if the players ship is destroyed.");
+            this.change_tutorial_text("If a checkpoint is reached they'll be marked with the colour of the player. The last reached checkpoint is used as a respawn point if the players ship is destroyed.");
         });
 
         tutorial_Button_Spacecrafts = GameObject.Find("Tutorial_Button_Spacecrafts").GetComponent<Button>();
@@ -107,7 +107,7 @@ public class Button_Help : MonoBehaviour
             this.un_highlight_Tutorial_Buttons();
             this.highlight_Tutorial_Button(tutorial_Button_Spacecrafts);
             this.change_tutorial_headline("Spacecrafts");
-            this.change_tutorial_text("The spacecrafts are used as the players avatar. Spacecrafts are equipped with weapons, a protective shield and extra fuel. The avatar can be moved with the use of the movement actions: Forward, Backward and Rotation.");
+            this.change_tutorial_text("Spacecrafts are used as the players avatar. Spacecrafts are equipped with weapons, a protective shield and extra fuel. The avatar can be moved with the use of the movement actions: Forward, Backward and Rotation.");
         });
 
         tutorial_Button_Planets = GameObject.Find("Tutorial_Button_Planets").GetComponent<Button>();
@@ -117,7 +117,7 @@ public class Button_Help : MonoBehaviour
             this.un_highlight_Tutorial_Buttons();
             this.highlight_Tutorial_Button(tutorial_Button_Planets);
             this.change_tutorial_headline("Planets");
-            this.change_tutorial_text("The Planets with thier gravity should be avoided at all cost. If a player crashes with a planet, the player will lose some of his health and respawn at the last claimed checkpoint.");
+            this.change_tutorial_text("The Planets with their gravity should be avoided at all cost. If a player crashes with a planet, the player will lose some of his health and respawn at the last claimed checkpoint.");
         });
 
         tutorial_Button_Movement = GameObject.Find("Tutorial_Button_Movement").GetComponent<Button>();
@@ -127,7 +127,7 @@ public class Button_Help : MonoBehaviour
             this.un_highlight_Tutorial_Buttons();
             this.highlight_Tutorial_Button(tutorial_Button_Movement);
             this.change_tutorial_headline("Movement");
-            this.change_tutorial_text("The movement actions: Forward, Backward, Rotation, allow the spacecraft to move through the galaxy. Each action costs fuel and the cost depends on the selected action. Forward and Backward can cost up to 6 fuel, while the cost of Rotation remain at 1 Fuel. The intensity of the movements are shown over the cards and the fuel cost are shown under the card. The motion can be slowed down bby using the opposite movement action.");
+            this.change_tutorial_text("The movement actions: Forward, Backward, Rotation, allow the spacecraft to move through the galaxy. Each action costs fuel and the cost depends on the selected action. Forward and Backward can cost up to 6 fuel, while the cost of Rotation remains at 1 Fuel. The intensity of the movements are shown over the cards and the fuel cost are shown under the card. The motion can be slowed down by using the opposite movement action.");
         });
 
         tutorial_Button_Weapons = GameObject.Find("Tutorial_Button_Weapons").GetComponent<Button>();
@@ -137,7 +137,7 @@ public class Button_Help : MonoBehaviour
             this.un_highlight_Tutorial_Buttons();
             this.highlight_Tutorial_Button(tutorial_Button_Weapons);
             this.change_tutorial_headline("Weapons");
-            this.change_tutorial_text("Weapons as rockets and mines can be collected through the galaxy. Weapons are used to damage the opponent or push them around.");
+            this.change_tutorial_text("Weapons as rockets and mines can be collected across the galaxy. Weapons are used to damage the opponent or push them around.");
         });
 
         tutorial_Button_Life = GameObject.Find("Tutorial_Button_Life").GetComponent<Button>();
@@ -147,7 +147,7 @@ public class Button_Help : MonoBehaviour
             this.un_highlight_Tutorial_Buttons();
             this.highlight_Tutorial_Button(tutorial_Button_Life);
             this.change_tutorial_headline("Health & Shield");
-            this.change_tutorial_text("The Health is shown at the bottom of the left corner and the shield is shown on the right corner. Health and shield decreases if you take damage and if the player loses all the health they'll lose the game.");
+            this.change_tutorial_text("The health is shown at the bottom of the left corner and the shield is shown on the right corner. Health and shield decreases if you take damage and if the player loses all the health they'll lose the game.");
         });
 
         tutorial_Button_Boost = GameObject.Find("Tutorial_Button_Boost").GetComponent<Button>();
@@ -157,7 +157,7 @@ public class Button_Help : MonoBehaviour
             this.un_highlight_Tutorial_Buttons();
             this.highlight_Tutorial_Button(tutorial_Button_Boost);
             this.change_tutorial_headline("Items");
-            this.change_tutorial_text("These items can be collected through the galaxy: \r\nRepair: restores up to 3 Health\r\nShield: restores up to 2 shield\r\nFuel: restores up to 5 Fuel");
+            this.change_tutorial_text("These items can be collected through the galaxy: \r\nRepair: restores up to 3 health\r\nShield: restores up to 2 shield\r\nFuel: restores up to 5 fuel");
         });
     }
 
