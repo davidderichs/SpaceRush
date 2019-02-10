872cab498a686c74629f63737cf0f5b361449d3e
Jakob <s67742@beuth-hochschule.de>
2019-02-10 11:36:11 +0100

    camera update: you can zoom in follow state now

--- SpaceRush/Assets/Scripts/Camera/CameraState/CameraStates/CameraFollowObjectsState.cs
+++ SpaceRush/Assets/Scripts/Camera/CameraState/CameraStates/CameraFollowObjectsState.cs
@@ -14,6 +14,8 @@ public class CameraFollowObjectsState : CameraState
     private float maxSize;
     private float aspectRation;
     private float tanFOV;
+    private float desiredZ;
+    private float zoomFactor;
 
     public CameraFollowObjectsState(CameraController camera, List<GameObject> gameObjects) : base(camera)
     {
@@ -24,6 +26,8 @@ public class CameraFollowObjectsState : CameraState
     {
         aspectRation = Screen.width / Screen.height;
         tanFOV = Mathf.Tan(Mathf.Deg2Rad * Camera.main.fieldOfView / 2f);
+        desiredZ = 0;
+        zoomFactor = 2;
     }
 
     public override void OnStateExit() { }
@@ -31,13 +35,31 @@ public class CameraFollowObjectsState : CameraState
     public override void Tick()
     {
         Vector3 camPos = camera.transform.position;
+
+        if (Input.mouseScrollDelta.y > 0 || Input.GetKey(KeyCode.Plus))
+        {
+            desiredZ = camPos.z / zoomFactor;
+        }
+        if (Input.mouseScrollDelta.y < 0 || Input.GetKey(KeyCode.Minus))
+        {
+            desiredZ = camPos.z * zoomFactor;
+        }
+
         Vector3 vecBetweenObjs = gameObjects[1].transform.position - gameObjects[0].transform.position;
         float distance = vecBetweenObjs.magnitude;
         Vector3 middle = gameObjects[0].transform.position + 0.5f * vecBetweenObjs;
         float camDist = (distance / 2.0f / aspectRation) / tanFOV;
         camPos.x = middle.x;
         camPos.y = middle.y;
-        camPos.z = middle.z - camDist - camera.extraDist;
+        float calculatedZ = middle.z - camDist - camera.extraDist;
+        if (desiredZ > calculatedZ)
+        {
+            camPos.z = calculatedZ;
+        }
+        else
+        {
+            camPos.z = desiredZ;
+        }
         camera.AnimateTo(camPos);
     }
 }
